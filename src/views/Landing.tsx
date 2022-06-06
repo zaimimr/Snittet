29b8673482e5e2fc9cb45b84e7fe3945a17bcbab
Zaim Imran <zaim.imran@gmail.com>
2022-06-06 13:48:55 +0200

    format

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -2,16 +2,14 @@ import { Container, Grid, Typography } from "@material-ui/core";
 import Card from "components/Card";
 import GradeCircle from "components/GradeCircle";
 import Searchbar from "components/Searchbar";
+import useSnackbar from "contexts/SnackbarContext";
 import { useSetStudies } from "contexts/StudiesContext";
 import React, { useEffect } from "react";
 import { IStudies } from "utils/types";
-import useSnackbar from "contexts/SnackbarContext";
 
 const Landing = () => {
   const [studies, setStudies] = useSetStudies();
   const { showSnackbar } = useSnackbar();
-  
-
 
   useEffect(() => {
     const storage_studies = localStorage.getItem("studies")

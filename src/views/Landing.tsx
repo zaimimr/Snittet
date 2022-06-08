15e6a919ccb184ff797d9d08c4d0423d19213a45
Zaim Imran <zaim.imran@gmail.com>
2022-06-08 11:07:46 +0200

    format

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -2,21 +2,18 @@ import { Container, Grid, Typography } from "@material-ui/core";
 import Card from "components/Card";
 import GradeCircle from "components/GradeCircle";
 import Searchbar from "components/Searchbar";
-import useSnackbar from "contexts/SnackbarContext";
 import { useSetStudies } from "contexts/StudiesContext";
 import React, { useEffect } from "react";
 import { IStudies } from "utils/types";
 
 const Landing = () => {
   const [studies, setStudies] = useSetStudies();
-  const { showSnackbar } = useSnackbar();
 
   useEffect(() => {
     const storage_studies = localStorage.getItem("studies")
     if (storage_studies) {
       setStudies(JSON.parse(storage_studies))
     }
-    showSnackbar("error", "Snitt kalkulator fungerer for øyeblikket ikke pga problemer https://grades.no. Prøv gjerne igjen om et par dager")
   }, []);
 
   return (

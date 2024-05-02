2abec9d1d1be8ea0f2551d4f004cc6eeedbd483d
Zaim Imran <zaim.imran@gmail.com>
2024-05-02 23:16:40 +0200

    fs

--- src/content/pages/index.val.ts
+++ src/content/pages/index.val.ts
@@ -14,5 +14,5 @@ export default c.define('/src/content/pages/index', schema, {
   metadataTitle: `Snittkalkulator`,
   metadataDescription: `En snittkalkulator for høyere utdanning, universiteter og høyskoler som henter emne data automatisk fra DBH for å gjøre det enklere for studenter å regne ut snittet sitt.`,
   title: `Hei og velkommen til den nye snittkalkulatoren! 🎉`,
-  description: `Etter at NTNU kuttet støtte for å hente fag fra deres APIer har snitt.zaim.no endret datakilde og støtter nå alle universiteter og høyskoler. Spread the word, den beste snittkalkulatoren er endelig her, klar for å hjelpe deg akkuratt deg 🎓`
+  description: `Etter at NTNU kuttet støtte for å hente fag fra deres APIer har snitt.zaim.no endret datakilde og støtter nå alle universiteter og høyskoler. Spread the word, den beste snittkalkulatoren er endelig her, klar for å hjelpe deg akkurat deg 🎓`
 });

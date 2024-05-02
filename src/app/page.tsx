8b1befcd787baa712cad20a99f9c95e5c0d3332b
Zaim Imran <zaim.imran@gmail.com>
2024-05-02 16:24:04 +0200

    finito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { IconButton } from '@radix-ui/themes';
+import { Callout, IconButton } from '@radix-ui/themes';
+import { fetchVal } from '@/val/val.rsc';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
 import DropdownSelect from '@/components/DropdownSelect';
+import IndexVal from '@/content/pages/index.val';
 import InfoCard from '@/components/InfoCard';
 import Link from 'next/link';
 import Subjects from '@/app/subjects';
-
-export default function Home() {
+export default async function Home() {
+  const { title, description } = await fetchVal(IndexVal);
   return (
     <main className="flex w-full flex-col items-center gap-4">
       <IconButton className="absolute right-4 top-4" asChild>
@@ -13,6 +15,13 @@ export default function Home() {
           <InfoCircledIcon height="15" width="15" />
         </Link>
       </IconButton>
+      <Callout.Root className="w-full">
+        <Callout.Icon>
+          <InfoCircledIcon />
+        </Callout.Icon>
+        <Callout.Text>{title}</Callout.Text>
+        <Callout.Text>{description}</Callout.Text>
+      </Callout.Root>
       <InfoCard />
       <DropdownSelect />
       <Subjects />
